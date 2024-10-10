abab159cf52d862b718373ba38c48ead2d513af4
Ehmah <ehmahmagomu7@gmail.com>
2024-10-10 18:04:40 +0300

    Add files via upload

---
+++ exercise.js
@@ -0,0 +1,47 @@
+/
+QN1.
+Var is used 
+Let is used 
+Const is used when variables shouldnt be reassigned.
+
+QN2.
+LET: you are able to reassign new values to variables
+
+CONST: you cannot reassign new values to variables
+
+QN3.
+It is an error ,specifically the "assignment to const variable error".
+
+QN4.
+Var because it enables us to reassign the values
+
+QN5.
+let studentname ="Magomu Emmanuel";
+const numberofstudents =13;
+console.log("Student name ",studentname);
+console.log("Number of students ",numberofstudents);
+
+QN6.
+  if ; used to declare the conditional statement 
+  condition
+  allows you to execute certain code blocks based on a condition.
+  It evaluates an expression, and if the expression is true, it executes a specified block of code.
+
+QN7.
+No output will be displayed
+This is because 
+
+QN8.let temperature=30;
+if temperature
+
+QN9.
+let age =18;
+if (age>=18) and (age<65){
+    console.log("You are eligible to work.");
+
+} else if (age>18) and (age>65){
+   console.log("You are of retirement age."):
+
+}
+   else{
+   console.log("You are too young to work")}
